4f083437d96e32e9b6dbb47d0ebea0c3d5b0facf
James Munger <jmunger@lakelandcollege.edu>
2021-01-04 23:02:06 +0000

    added some repl.it directions to main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#Step 1, edit the input.txt to include your quizzes and questions
+#step 2, click Run at the top
+#step 3, click the 3 dots above files and download zip
+#step 4, retrieve the newquiz.zip
+#step 5 import into your course as a QTI object
+#all done
 from question import Quizzes
 import sys
 
@@ -7,10 +13,6 @@ if len(sys.argv) > 1:
   input_file = sys.argv[1]
  
 theQuiz = Quizzes(input_file)
-
-#get rid of last job's files/folders
-# theQuiz.create_export_file()
-#now to output
 
 
 
